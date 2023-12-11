8c26cd914498065239ab65cfcb5693b5962dfb71
Asadbek <ibnnumon@gmail.com>
2023-12-11 12:19:08 +0500

    feat: dark and light mode

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <script setup>
 const color = useColorMode();
 
-
 function toggleColorMode() {
     color.value = color.value === 'dark' ? 'light' : 'dark'
 }
 </script>
 
 <template>
-    <h1>Nuxt</h1>
-    <button @click="toggleColorMode">{{ color.value }}</button>
+    <button @click="toggleColorMode">
+        <div class='i-carbon-moon dark:i-carbon-sun'></div>
+    </button>
 </template>
 
--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig, presetUno } from 'unocss'
+import { defineConfig, presetIcons, presetUno } from 'unocss'
 
 export default defineConfig({
     shortcuts: {
         // global style in here
     },
     presets: [
-        presetUno()
+        presetUno(),
+        presetIcons()
     ]
 })
